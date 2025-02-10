5ea1746e55c7d0ddab54c9dbf5dc37e2858c921b
Carlos Ismael Orozco Rios <carloselcior@gmail.com>
2025-02-10 09:53:35 -0700

    2.5

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,29 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = (props) => (
-    <div>
-        {
-        props.parts.map(part =>
-            <Part key={part.id} part={part} />
-        )}
-    </div>
-)
-
-const Part = (props) => (
-    <p>
-        {props.part.name} {props.part.exercises}
-    </p>
-)
-
-const Total = (props) => <p><b>Total excercises: {props.parts.reduce((acc, part) => acc + part.exercises, 0)}</b></p>
-
-const Course = ({ course }) => {
-    return (<div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-    </div>)
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
