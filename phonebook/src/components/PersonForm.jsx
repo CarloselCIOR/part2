7dbdcf4769f0ea627c0bb07e97ca41f1d15e4fb8
Carlos Ismael Orozco Rios <carloselcior@gmail.com>
2025-02-12 12:55:17 -0700

    2.10

---
+++ phonebook/src/components/PersonForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const PersonForm = ({ persons={persons}, setPersons={setPersons} }) => {
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const newPerson = (event) => {
+        event.preventDefault()
+        if (persons.find(person => person.name === newName)) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            const nameObject = {
+                name: newName,
+                number: newNumber
+            }
+
+            setPersons(persons.concat(nameObject))
+            setNewName('')
+            setNewNumber('')
+        }
+    }
+
+    return (
+        <form onSubmit={newPerson}>
+            <div>name: <input value={newName} onChange={handleNameChange} /></div>
+            <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+            <div><button type="submit">add</button> </div>
+        </form>
+    );
+};
+
+export default PersonForm
